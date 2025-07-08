942586af02b50bac8d7301d521bb160a5e5c0d2c
Gobinda17 <gobindadeb14@gmail.com>
2025-07-08 17:35:16 +0530

    Creating Routes, Controller, Middleware, Models

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const router = require("./routes/userRoutes");
+
 app.use(express.json());
+
+app.use("/event-management/api/v1/user", router);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 require("dotenv").config();
 
+const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGO_URI;
+
 const app = require("./app");
 const PORT = process.env.PORT;
 
-app.listen(PORT, (error) => {
-    if(error) {
-        return console.error("❌ Something went wrong!!!", error)
-    }
-    console.log(`✅ Server is listening in ${PORT}`);
-});
 
+(async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("✅ Connected to MongoDB");
+
+        app.listen(PORT, (error) => {
+            if (error) {
+                return console.error("❌ Something went wrong!!!", error)
+            }
+            console.log(`✅ Server is listening in ${PORT}`);
+        });
+    } catch (error) {
+        console.error("❌ Error connecting to MongoDB:", error);
+    }
+})();
