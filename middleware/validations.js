fd4aeba6c95438fd28d7448c5ea53ad73f94516d
Gobinda17 <gobindadeb14@gmail.com>
2025-07-10 00:55:10 +0530

    Schema Controller and Middleware added for Registration

--- middleware/validations.js
+++ middleware/validations.js
@@ -5,7 +5,7 @@ const registrationValidation = [
     body('email').notEmpty().isEmail().withMessage('Email is required.' || 'Email is invalid.'),
     body('phone').notEmpty().isLength({ max: 10 }).withMessage('Phone number is required.' || 'Phone number is invalid.'),
     body('password').notEmpty().isLength({ min: 6 }).withMessage('Password is required.' || 'Password length must be at least 6 characters long.'),
-    param('role').isIn(['user', 'organiser']).withMessage('Role must be either "user" or "organizer"')
+    param('role').isIn(['user', 'organiser']).withMessage('Role must be either "user" or "organiser"')
 ];
 
 module.exports = registrationValidation;
